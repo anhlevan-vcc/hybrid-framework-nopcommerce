eacfe582987fcec86aa10db2f1f074176185767a
LeVanAnh <levananhck2.1@gmail.com>
2022-09-27 23:40:43 +0700

    Update_Eclipse

--- javaTester/javaAccModFirst/Computer.java
+++ javaTester/javaAccModFirst/Computer.java
@@ -6,23 +6,30 @@ public class Computer {
 	protected int eyeNumber; // màu vàng
 	public float weight; // màu xanh lá cây
 
-	// THUỘC TÍNH - Property
-
+	// Property(thuộc tính)
 	private int ramSize;// biến toàn cục
+	String ramName;
 
-	// PHƯƠNG THỨC - Method
+	// Method(Phương thức)
 	private void setRamSize(int ramSize) { // ramSize- biến cục bộ
 		// đặt cùng tên thì dùng this để lấy ra biến toàn cục
 		// không dùng this thì viết tên khác nhau đi
 		this.ramSize = ramSize;
 	}
 
+	void setRamProductName(String ramName) {
+		this.ramName = ramName;
+
+	}
+
 	public static void main(String[] args) {
 		Computer comp = new Computer();
-		comp.ramSize = 5;
 
+		// Property(thuộc tính)
+		comp.ramSize = 5;
 		System.out.println(comp.ramSize);
 
+		// Method(Phương thức)
 		comp.setRamSize(6);
 		System.out.println(comp.ramSize);
 	}
--- javaTester/javaAccModSecond/Pc_Co_KeThua.java
+++ javaTester/javaAccModSecond/Pc_Co_KeThua.java
@@ -1,6 +1,7 @@
 package javaAccModSecond;
 
+import javaAccModFirst.Computer;
 
-public class Pc_Co_KeThua {
-	
+public class Pc_Co_KeThua extends Computer {
+
 }
