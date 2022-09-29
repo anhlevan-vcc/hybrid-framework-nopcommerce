f5323600ce6d27daa75ee4cf4339d3b054b31abb
Le Van Anh <levananhck2.1@gmail.com>
2022-09-29 17:12:20 +0700

    Fw02_DRY_KISS_YAGNI Pattern

---
+++ testcases/com/nopcommerce/user/User_01_Register_Dry.java
@@ -0,0 +1,121 @@
+package com.nopcommerce.user;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+
+public class User_01_Register_Dry {
+	WebDriver driver;
+	String emailValid;
+	String projectPath = System.getProperty("user.dir");
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+
+		emailValid = "Automation" + generateRandomNumber() + "@gmail.com";
+	}
+
+	@Test
+	public void TC_01() {
+		driver.get("https://demo.nopcommerce.com/");
+		driver.findElement(By.xpath("//a[text()= 'Register']")).click();
+		driver.findElement(By.cssSelector("button#register-button")).click();
+		Assert.assertEquals(driver.findElement(By.cssSelector("span#FirstName-error")).getText(), "First name is required.");
+		Assert.assertEquals(driver.findElement(By.cssSelector("span#LastName-error")).getText(), "Last name is required.");
+		Assert.assertEquals(driver.findElement(By.cssSelector("span#Email-error")).getText(), "Email is required.");
+		Assert.assertEquals(driver.findElement(By.cssSelector("span#Password-error")).getText(), "Password is required.");
+		Assert.assertEquals(driver.findElement(By.cssSelector("span#ConfirmPassword-error")).getText(), "Password is required.");
+
+	}
+
+	@Test
+	public void TC_02() {
+		driver.findElement(By.xpath("//a[text()= 'Register']")).click();
+		driver.findElement(By.cssSelector("input#FirstName")).sendKeys("Automation");
+		driver.findElement(By.cssSelector("input#LastName")).sendKeys("FC");
+		driver.findElement(By.cssSelector("input#Email")).sendKeys("123@123@123");
+		driver.findElement(By.cssSelector("input#Password")).sendKeys("123456");
+		driver.findElement(By.cssSelector("input#ConfirmPassword")).sendKeys("123456");
+		driver.findElement(By.cssSelector("button#register-button")).click();
+		Assert.assertEquals(driver.findElement(By.cssSelector("span#Email-error")).getText(), "Wrong email");
+	}
+
+	@Test
+	public void TC_03() {
+		driver.findElement(By.xpath("//a[text()= 'Register']")).click();
+		driver.findElement(By.cssSelector("input#FirstName")).sendKeys("Automation");
+		driver.findElement(By.cssSelector("input#LastName")).sendKeys("FC");
+		driver.findElement(By.cssSelector("input#Email")).sendKeys(emailValid);
+		driver.findElement(By.cssSelector("input#Password")).sendKeys("123456");
+		driver.findElement(By.cssSelector("input#ConfirmPassword")).sendKeys("123456");
+		driver.findElement(By.cssSelector("button#register-button")).click();
+		Assert.assertEquals(driver.findElement(By.cssSelector("div.result")).getText(), "Your registration completed");
+		driver.findElement(By.cssSelector("a.ico-logout")).click();
+	}
+
+	@Test
+	public void TC_04() {
+		driver.findElement(By.xpath("//a[text()= 'Register']")).click();
+		driver.findElement(By.cssSelector("input#FirstName")).sendKeys("Automation");
+		driver.findElement(By.cssSelector("input#LastName")).sendKeys("FC");
+		driver.findElement(By.cssSelector("input#Email")).sendKeys("automationfc.vn@gmail.com");
+		driver.findElement(By.cssSelector("input#Password")).sendKeys("123456");
+		driver.findElement(By.cssSelector("input#ConfirmPassword")).sendKeys("123456");
+		driver.findElement(By.cssSelector("button#register-button")).click();
+		Assert.assertEquals(driver.findElement(By.cssSelector("div.message-error li")).getText(), "The specified email already exists");
+	}
+
+	@Test
+	public void TC_05() {
+		driver.findElement(By.xpath("//a[text()= 'Register']")).click();
+		driver.findElement(By.cssSelector("input#FirstName")).sendKeys("Automation");
+		driver.findElement(By.cssSelector("input#LastName")).sendKeys("FC");
+		driver.findElement(By.cssSelector("input#Email")).sendKeys(emailValid);
+		driver.findElement(By.cssSelector("input#Password")).sendKeys("123");
+		driver.findElement(By.cssSelector("input#ConfirmPassword")).sendKeys("123");
+		driver.findElement(By.cssSelector("button#register-button")).click();
+		Assert.assertEquals(driver.findElement(By.cssSelector("span#Password-error")).getText(), "Password must meet the following rules:\nmust have at least 6 characters");
+	}
+
+	@Test
+	public void TC_06() {
+		driver.findElement(By.xpath("//a[text()= 'Register']")).click();
+		driver.findElement(By.cssSelector("input#FirstName")).sendKeys("Automation");
+		driver.findElement(By.cssSelector("input#LastName")).sendKeys("FC");
+		driver.findElement(By.cssSelector("input#Email")).sendKeys(emailValid);
+		driver.findElement(By.cssSelector("input#Password")).sendKeys("123456");
+		driver.findElement(By.cssSelector("input#ConfirmPassword")).sendKeys("654321");
+		driver.findElement(By.cssSelector("button#register-button")).click();
+		Assert.assertEquals(driver.findElement(By.cssSelector("span#ConfirmPassword-error")).getText(), "The password and confirmation password do not match.");
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+	public void sleepInSecond(long timeInSecond) {
+		try {
+			Thread.sleep(timeInSecond * 1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public int generateRandomNumber() {
+		Random rand = new Random();
+		return rand.nextInt(9999);
+	}
+}
