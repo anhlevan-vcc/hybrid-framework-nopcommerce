fb543ff457b1361adf8b2dc5abf9c8c6a4a0da7d
Le Van Anh <levananhck2.1@gmail.com>
2023-02-07 18:04:24 +0700

    Grid

---
+++ startNote/startNode.bat
@@ -0,0 +1,2 @@
+set ProjectPath=%~dp0
+java -jar -Dwebdriver.chrome.driver="%ProjectPath%\chromedriver.exe" -Dwebdriver.gecko.driver="%ProjectPath%\geckodriver.exe" selenium-server-standalone-3.141.59.jar -role webdriver -hub http://192.168.20.164:4444/grid/register -port 5555
